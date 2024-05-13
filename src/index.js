275c65000e8cc7c954ee64d87d35dc34c757eb64
mashudutshifura <164932719+mashudutshifura@users.noreply.github.com>
2024-05-14 00:37:54 +0200

    added id class for the icon, added the icon element on index.js, amended the basic CSS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function displayTemperature(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -14,6 +15,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temeprature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
@@ -35,9 +37,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
   return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
